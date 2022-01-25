5ca77544e42b1f52aeb56d6e5dec44653cf783b9
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-01-24 21:58:56 -0300

    Final Layout
    Terminando o layout das telas.

---
+++ ShoppingUsa/ShoppingUsa/TaxesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaxesViewController.swift
+//  ShoppingUsa
+//
+//  Created by Rennan Bruno on 24/01/22.
+//
+
+import UIKit
+
+class TaxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
